f0e4e30650600416fdb884ed160f2951d9af74bc
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-10-13 18:37:12 +0530

    Update logomaker.py

--- TGNRobot/modules/logomaker.py
+++ TGNRobot/modules/logomaker.py
@@ -20,27 +20,27 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./TGNRobot/resources/blackbg.jpg.jpeg')
+    img = Image.open('./TGNRobot/resources/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./TGNRobot/resources/Deadly Advance.otf", 330)
+    font = ImageFont.truetype("./TGNRobot/resources/Chopsic.otf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="white", stroke_width=10, stroke_fill="blue")
+    draw.text((x, y), text, font=font, fill="white", stroke_width=25, stroke_fill="red")
     fname2 = "LogoByzaid.png"
     img.save(fname2, "png")
     await tbot.send_file(event.chat_id, fname2, caption="✌️✨")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @electrobot_support, {e}')
+   await event.reply(f'Error Report @godfathersupport, {e}')
 
 
 
